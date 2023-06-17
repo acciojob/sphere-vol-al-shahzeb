f0ed24b8e578bec90bab27eeae638761e8f530a4
al-shahzeb <99374818+al-shahzeb@users.noreply.github.com>
2023-06-17 13:25:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+
+	// document.getElementById('MyForm')
+
+	const radius = document.getElementById('radius').innerText;
+	const volEl = document.getElementById('volume');
+	const volume = (4/3)*Math.PI*radius^3;
+
+	volEl.innerText = volume;
   
 } 
 
